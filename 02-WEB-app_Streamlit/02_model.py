4e4ed38f1593aa09f30cb9b4346dd3e57cd7188e
Vlad A <vladevo6@gmail.com>
2024-01-29 05:45:23 +0000

    тестовая загруза модели из 2 модуля

---
+++ 02-WEB-app_Streamlit/02_model.py
@@ -0,0 +1,15 @@
+import io
+import streamlit as st
+from transformers import pipeline
+import numpy as np
+
+asr = pipeline("automatic-speech-recognition", "openai/whisper-small")
+
+uploaded_file = st.file_uploader("Upload a file")
+
+if uploaded_file:
+    st.write("Filename: ", uploaded_file.name)
+    file_to_model = uploaded_file.getvalue()
+
+    text = asr(file_to_model)
+    st.write(text)

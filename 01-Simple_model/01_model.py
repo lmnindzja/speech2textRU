a8c0cb4e0dd46af030a998f6703637448ae60337
lmnindzja <89553548+lmnindzja@users.noreply.github.com>
2024-01-31 05:12:03 +0300

    Update 01_model.py

--- 01-Simple_model/01_model.py
+++ 01-Simple_model/01_model.py
@@ -1,9 +1,9 @@
 from transformers import pipeline
-!wget -O uploaded.mp3 http://www.moviesoundclips.net/movies1/backtothefuture/style.mp3
+audio_link="http://www.moviesoundclips.net/movies1/backtothefuture/style.mp3"
 asr = pipeline("automatic-speech-recognition",   
                       "openai/whisper-small")
 
 
-text = asr('uploaded.mp3')
+text = asr(audio_link)
 print (text)
 

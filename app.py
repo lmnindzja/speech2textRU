19445e0e9380913faab9928b5f7feb855b6aea37
lmnindzja <89553548+lmnindzja@users.noreply.github.com>
2024-01-31 03:54:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-!pip install huggingsound
-from huggingsound import SpeechRecognitionModel
+import io
+import streamlit as st
+from transformers import pipeline
+import numpy as np
+import ffmpeg
 
-model = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-russian")
-audio_paths = ["/var/tmp/voice.mp3"]
+asr = pipeline("automatic-speech-recognition", "openai/whisper-small")
 
-transcriptions = model.transcribe(audio_paths)
-print(transcriptions)
+uploaded_file = st.file_uploader("Upload a file")
+
+if uploaded_file:
+    st.write("Filename: ", uploaded_file.name)
+    file_to_model = uploaded_file.getvalue()
+
+    text = asr(file_to_model)
+    st.write(text)

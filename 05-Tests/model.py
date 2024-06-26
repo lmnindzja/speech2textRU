27c151cb446e0b3cee28fb6bbe9c18471f52b9a3
lmnindzja <89553548+lmnindzja@users.noreply.github.com>
2024-06-26 03:40:00 +0300

    Update model.py
    ./05-Tests/model.py:1:1: F401 'io' imported but unused
    ./05-Tests/model.py:5:1: F401 'ffmpeg' imported but unused
    ./05-Tests/model.py:9:1: E302 expected 2 blank lines, found 1
    ./05-Tests/model.py:19:36: E261 at least two spaces before inline comment
    ./05-Tests/model.py:19:37: E262 inline comment should start with '# '
    ./05-Tests/model.py:25:1: E305 expected 2 blank lines after class or function definition, found 1

--- 05-Tests/model.py
+++ 05-Tests/model.py
@@ -1,17 +1,19 @@
-import io
 import streamlit as st
 from transformers import pipeline
 import requests
-import ffmpeg
 
 asr = pipeline("automatic-speech-recognition", "openai/whisper-small")
 
+
 def asr_app_from_audio_file(audio_file_path):
+
+    
     st.write("Processing audio from file: ", audio_file_path)
 
     try:
         audio_response = requests.get(audio_file_path)
-        audio_response.raise_for_status()  # Проверка наличия ошибок при получении звука
+        audio_response.raise_for_status() 
+# Проверка наличия ошибок при получении звука
     except requests.exceptions.RequestException as e:
         st.error(f"Error fetching audio from file: {e}")
         return

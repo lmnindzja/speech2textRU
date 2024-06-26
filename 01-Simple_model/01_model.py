4368f02bb5be6af39f321f5569073c4afe0bbc29
lmnindzja <89553548+lmnindzja@users.noreply.github.com>
2024-06-26 03:17:01 +0300

    Update 01_model.py
    pep8 fix
    
    ./01-Simple_model/01_model.py:2:11: E225 missing whitespace around operator
    ./01-Simple_model/01_model.py:4:23: E127 continuation line over-indented for visual indent
    ./01-Simple_model/01_model.py:8:6: E211 whitespace before '('

--- 01-Simple_model/01_model.py
+++ 01-Simple_model/01_model.py
@@ -1,8 +1,5 @@
 from transformers import pipeline
-audio_link="http://www.moviesoundclips.net/movies1/backtothefuture/style.mp3"
-asr = pipeline("automatic-speech-recognition",
-                      "openai/whisper-small")
-
-
+audio_link = "http://www.moviesoundclips.net/movies1/backtothefuture/style.mp3"
+asr = pipeline("automatic-speech-recognition", "openai/whisper-small")
 text = asr(audio_link)
-print (text)
+print(text)
